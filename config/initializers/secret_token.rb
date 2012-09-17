638051f53e8a3abfe4087f78099c8272000c81a3
Reizinixc <Reizinixc@reireiz.tk>
2012-09-17 20:28:15 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OHIHA::Application.config.secret_token = '09bb06ea2e2299eadab7d671ccc94fe2a7976c196b4128415380d7364cdef6087cfb7150649c491e3945b6a6e04c971dfc155735e49ac452926995c72c84f101'
